e6953f9cbe7bec970661790bd6d826938b09aef9
Jeffrey I. Schiller <jis@mit.edu>
2014-08-20 11:47:41 -0400

    StartSystem works.
    Change-Id: I67ca6ff26f7c85babde25f69191a2f7668abbe2d

--- appinventor/starter/src/edu/mit/appinventor/StartSystem.java
+++ appinventor/starter/src/edu/mit/appinventor/StartSystem.java
@@ -1,9 +1,54 @@
 package edu.mit.appinventor;
 
 import java.io.File;
+import java.io.IOException;
 
 public class StartSystem {
     public static void main(String [] argv) {
-      System.out.println(new File(StartSystem.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
+      File execDir = new File(new File(StartSystem.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent());
+      Process s = null;
+      Process build = null;
+      ProcessBuilder server = new ProcessBuilder("java", "-jar", "jetty-runner.jar", "--port", "8888", "appinventor.xml");
+      server.inheritIO();
+      server.directory(execDir);
+
+      File buildserverLibs = new File(execDir.getPath() + "/buildserver");
+      File [] fileList = buildserverLibs.listFiles();
+      if (fileList == null) {
+          System.err.println("Could not find buildserver libraries.");
+          System.exit(1);
+      }
+      String cp = "";
+      boolean first = true;
+      for (File file: fileList) {
+          if (file.isFile()) {
+              if (first) {
+                  cp += file.getPath();
+                  first = false;
+              } else {
+                  cp += ":" + file.getPath();
+              }
+          }
+      }
+
+      ProcessBuilder buildserver = new ProcessBuilder("java", "-Xmx1828m", "-cp", cp, "com.google.appinventor.buildserver.BuildServer",
+        "--dexCacheDir", "/tmp/dxcache");
+      buildserver.inheritIO();
+      buildserver.directory(execDir);
+
+      try {
+          s = server.start();
+          build = buildserver.start();
+      } catch (IOException e) {
+          System.exit(1);
+      }
+      try {
+          s.waitFor();
+          build.waitFor();
+      } catch (InterruptedException e) {
+      }
+
+
     }
+
 }
